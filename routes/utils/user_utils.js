eb776eb59e92ce731e402a3d8389a9d4bc92620d
Nivfono2001 <81295283+Nivfono2001@users.noreply.github.com>
2022-06-11 17:12:21 +0300

    same

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -12,6 +12,21 @@ async function markAsFavorite(user_name, recipe_id){
     
 }
 
+
+//NIV DONE
+//RETURNS ALL RECIPES OF SPECIFIC USER BY USERNAME
+// async function getUserRecipes(user_name){
+//     try{
+//        const recipes_id= await DButils.execQuery(`select recipe_id from users where user_name='${user_name}'`);
+//        return recipes_id; 
+//     }
+//     catch(err){
+//        throw { status: 401, message: err };
+//     }
+    
+// }
+
+
 async function getFavoriteRecipes(user_name){
     try{
         const recipes_id = await DButils.execQuery(`select recipe_id from favorites where user_name='${user_name}'`);
@@ -36,6 +51,19 @@ async function getFamilyRecipesFromDb(user_name){
 
 }
 
+//NIV DONE
+//insert recipe to user
+// async function addRecipeToUser(user_name, recipe_id){
+//     try{
+//         await DButils.execQuery(`insert into myrecipes values ('${user_name}', '${recipe_id}', '${owner}', '${when_to_cook}', '${ingredients}', '${instructions}',' ${photos}')`);
+//     }
+//     catch(err){
+//         throw { status: 401, message: err };
+//     }
+// }
+
+
+
 async function addFamilyRecipeToDb(user_name, recipe_id, owner, when_to_cook, ingredients, instructions, photos){
     try{
         await DButils.execQuery(`insert into familyrecipes values ('${user_name}', '${recipe_id}', '${owner}', '${when_to_cook}', '${ingredients}', '${instructions}',' ${photos}')`);
@@ -49,3 +77,6 @@ exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
 exports.getFamilyRecipesFromDb = getFamilyRecipesFromDb;
 exports.addFamilyRecipeToDb = addFamilyRecipeToDb;
+//NIV DONE
+//exports.getUserRecipes=getUserRecipes;
+//exports.addRecipeToUser=addRecipeToUser
